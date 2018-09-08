996e4c11d51fad197c1079f6eac04cd9d5de1165
Levan <levana.xue@hotmail.com>
2018-09-08 13:16:08 +0800

    http request 全局处理 401/ 403/ 0

--- src/app/core/interceptors/http.token.interceptor.ts
+++ src/app/core/interceptors/http.token.interceptor.ts
@@ -5,14 +5,21 @@ import {
   HttpHandler,
   HttpRequest
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { JwtService } from '../services';
+import { Observable, of } from 'rxjs';
+import { JwtService, MissionInteractionService } from '../services';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { Message } from 'primeng/components/common/message';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpTokenInterceptor implements HttpInterceptor {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private router: Router,
+    private jwtService: JwtService,
+    private missionInteractionService: MissionInteractionService
+  ) {}
 
   intercept(
     req: HttpRequest<any>,
@@ -28,6 +35,36 @@ export class HttpTokenInterceptor implements HttpInterceptor {
       headersConfig['authorization'] = token;
     }
     const request = req.clone({ setHeaders: headersConfig });
-    return next.handle(request);
+    return next.handle(request).pipe(catchError((error, caught) => {
+      if (error) {
+        let errMessage: Message;
+        switch (error.status) {
+          case 401:
+            errMessage = {
+              severity: 'warn',
+              summary: '系统提示',
+              detail: '未授权访问，请重新登入！'
+            };
+            this.router.navigateByUrl('user/login');
+            break;
+          case 403:
+            errMessage = {
+              severity: 'warn',
+              summary: '系统提示',
+              detail: '无权限访问！'
+            };
+            break;
+          case 0:
+            errMessage = {
+              severity: 'error',
+              summary: '网络错误',
+              detail: '未连接至服务器，请检查网络连接！'
+            };
+            break;
+        }
+        if (errMessage) this.missionInteractionService.notification(errMessage);
+      }
+      return of(error);
+    }) as any);
   }
 }
